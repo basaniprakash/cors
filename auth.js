80c71abb7ea5216606cf9d1160e280f9a6886449
Bhanuprakash Basani <bhanuprakash.basani@gmail.com>
2023-10-08 12:47:42 +0530

    works with google auth

--- auth.js
+++ auth.js
@@ -5,6 +5,8 @@ const app = express();
 const session = require('express-session');
 const https = require('https');
 const fs = require('fs');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // SSL config
 const privateKey = fs.readFileSync('certs/auth.key', 'utf8');
@@ -32,6 +34,54 @@ app.use(cors({
 
 app.use(csurf());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+	done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+	clientID: ''
+	clientSecret: ''
+	callbackURL: 'https://www.corsauth.com/auth/callback'
+	}, (token, tokenSecret, profile, done) => {
+	return done(null, profile);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['https://www.googleapis.com/auth/plus.login']
+}));
+
+app.get('/auth/callback', (req, res, next) => {
+    passport.authenticate('google', (err, user, info) => {
+        if (err) {
+            console.error("Authentication error:", err);
+            return next(err);
+        }
+        if (!user) {
+            console.log("Authentication failed:", info.message);
+            return res.redirect('/'); // Failed auth, so maybe stay on the auth page or go to an error page.
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                console.error("Login error:", err);
+                return next(err);
+            }
+            return res.redirect('https://www.corsmainapp.com/complete-auth');  // Redirect to the main app on successful authentication.
+        });
+    })(req, res, next);
+});
+
+app.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+
 app.get('/start-auth', (req, res) => {
     console.log('/start-auth');
 	const token = 'some_generated_token';
@@ -45,6 +95,15 @@ app.get('/start-auth', (req, res) => {
 	});
     //res.redirect(redirectUrl);
 });
+
+app.get('/favicon.ico', (req, res) => {
+    res.send('favicon');
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Cors Auth!');
+});
+
 
 // Starting the server with HTTPS
 const host = 'www.corsauth.com';
--- ui.js
+++ ui.js
@@ -1,24 +1,8 @@
 const authButton = document.getElementById('authButton');
 
 authButton.addEventListener('click', () => {
-    // Fetch the redirect URL from the auth service
-    fetch('https://www.corsauth.com/start-auth', {
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include'
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Check if the redirect URL is present in the response
-        if (data.redirectURL) {
-            // Navigate to the redirect URL
-            window.location.href = data.redirectURL;
-        } else {
-            console.error('Redirect URL missing in the response.');
-        }
-    })
-    .catch(error => {
-        console.error('Failed to start auth:', error);
-    });
+    // Simply redirect to the Google authentication URL
+    window.location.href = 'https://www.corsauth.com/auth/google';
 });
+
 
